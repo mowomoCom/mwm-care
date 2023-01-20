3496df3174908fcb33402e99b3b95ad73b9dc99a
Pablo Miralles <pablo@mowomo.com>
2023-01-20 10:15:33 +0100

    fix(js window)

--- assets/js/admin_scripts.js
+++ assets/js/admin_scripts.js
@@ -1,3 +1,11 @@
 jQuery(window).load(function () {
 
+	/**
+	 * We change the tab to the one before reloading the page.
+	 */
+	if (window.location.hash) {
+		jQuery('.mwm_admin_page .nav-tab[href="' + window.location.hash + '"]').trigger('click');
+	}
+
+
 });
